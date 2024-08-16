21d7390c60f4f15d64e6f4dcd749cdd1a0934c53
RaisaNuzhat <raisa.csecu@gmail.com>
2024-08-16 20:35:53 +0600

    setup done

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+//  middlewares
+const corsOptions = {
+    origin: [
+      "http://localhost:5173",
+      "https://th-assignment-a87d3.web.app",
+      "https://th-assignment-a87d3.firebaseapp.com",
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  };
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.9odt6wv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    const itemCollection = client.db("quixo").collection("items");
+    // Connect the client to the server	(optional starting in v4.7)
+    //await client.connect();
+    // Send a ping to confirm a successful connection
+    //await client.db("admin").command({ ping: 1 });
+    //console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
+  app.use(cors(corsOptions));
+  app.use(express.json());
